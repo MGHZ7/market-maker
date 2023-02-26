b86704c10041e560a8a74a534c079f7f9026c972
Motasem Ghozlan <Motasem.go.26@gmail.com>
2023-02-26 02:54:15 +0200

    Card text truncate

--- src/components/ui/card/itemCard/itemCard.tsx
+++ src/components/ui/card/itemCard/itemCard.tsx
@@ -2,17 +2,27 @@ import { Button } from "../../button";
 import { ImageProps } from "../../image";
 import { MediaCard } from "../mediaCard/mediaCard";
 
+import TextTruncate from 'react-text-truncate'; // recommend
+
 export interface ItemCardProps {
     image: ImageProps;
     title: string;
     description: string;
 };
 
+const NUMBER_OF_DESCRIPTION_LINES = 2;
+
 export const ItemCard = ({ image, title, description }: ItemCardProps) => {
 
     const Body = () => (<div className={``}>
         <h4 className={`text-lg truncate`}>{title}</h4>
-        <p className={`text-sm text-ellipsis overflow-hidden h-10`} dangerouslySetInnerHTML={{ __html: description }}></p>
+        <TextTruncate
+            line={NUMBER_OF_DESCRIPTION_LINES}
+            element="div"
+            truncateText="…"
+            text={description}
+            containerClassName={`text-sm text-ellipsis overflow-hidden h-10`}
+        />
     </div>);
 
     const Footer = () => <div className={`flex justify-center`}>
